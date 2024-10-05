2fc96a51ca1b70843f89dd84914353bc0b3c850d
oructuran0 <orctrn193@gmail.com>
2024-10-05 04:53:15 +0300

    Servislerim alani admin paneli ve ui da dinamik hale getirildi

---
+++ MyPortfolioProject/Controllers/ServiceController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyPortfolioProject.Models;
+
+namespace MyPortfolioProject.Controllers
+{
+    public class ServiceController : Controller
+    {
+        DbMyPortfolioEntities context = new DbMyPortfolioEntities();
+        public ActionResult ServiceList()
+        {
+            var values = context.Service.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult CreateService()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateService(Service service)
+        {
+            context.Service.Add(service);
+            context.SaveChanges();
+            return RedirectToAction(nameof(ServiceList));
+        }
+        public ActionResult DeleteService(int id)
+        {
+            var value = context.Service.Find(id);
+            context.Service.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction(nameof(ServiceList));
+        }
+        [HttpGet]
+        public ActionResult UpdateService(int id)
+        {
+            var value = context.Service.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateService(Service service)
+        {
+            var value = context.Service.Find(service.ServiceID);
+            value.Icon = service.Icon;
+            value.Title = service.Title;
+            value.Description = service.Description;
+            value.SubTitleA = service.SubTitleA;
+            value.SubTitleB = service.SubTitleB;
+            value.SubTitleC = service.SubTitleC;
+            context.SaveChanges();
+            return RedirectToAction(nameof(ServiceList));
+        }
+    }
+}
