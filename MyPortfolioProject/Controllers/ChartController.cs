e7ae629d220e468b81e86c0032462c3286286988
oructuran0 <orctrn193@gmail.com>
2024-10-05 17:38:27 +0300

    Admin tarafina Chart alani eklendi ve Yetenekler grafiklendirildi

---
+++ MyPortfolioProject/Controllers/ChartController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyPortfolioProject.Models;
+
+namespace MyPortfolioProject.Controllers
+{
+    public class ChartController : Controller
+    {
+        // GET: Chart
+        DbMyPortfolioEntities context = new DbMyPortfolioEntities();
+        public ActionResult GetSkills()
+        {
+            var values = context.Skill.ToList();
+            return PartialView(values);
+        }
+    }
+}
