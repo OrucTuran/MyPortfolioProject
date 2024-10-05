6516a526086ca73797923f70b2731ef6d0096602
oructuran0 <orctrn193@gmail.com>
2024-10-05 05:17:33 +0300

    Sosyal Medya alani dinamik hale getirildi

---
+++ MyPortfolioProject/Controllers/SocialMediaController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyPortfolioProject.Models;
+
+namespace MyPortfolioProject.Controllers
+{
+    public class SocialMediaController : Controller
+    {
+        // GET: SocialMedia
+        DbMyPortfolioEntities context = new DbMyPortfolioEntities();
+        public ActionResult Index()
+        {
+            var values = context.SocialMedia.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult CreateSocialMedia()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateSocialMedia(SocialMedia socialMedia)
+        {
+            context.SocialMedia.Add(socialMedia);
+            context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        public ActionResult DeleteSocialMedia(int id)
+        {
+            var value = context.SocialMedia.Find(id);
+            context.SocialMedia.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public ActionResult UpdateSocialMedia(int id)
+        {
+            var value = context.SocialMedia.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateSocialMedia(SocialMedia socialMedia)
+        {
+            var value = context.SocialMedia.Find(socialMedia.SocialMediaID);
+            value.Title = socialMedia.Title;
+            value.URL = socialMedia.URL;
+            value.Icon = socialMedia.Icon;
+            context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
